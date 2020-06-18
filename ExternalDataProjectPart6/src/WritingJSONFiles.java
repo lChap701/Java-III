7118c7e70048a3987748ca5896f33840268f89bf
lChap701 <57417221+lChap701@users.noreply.github.com>
2020-06-18 11:08:56 -0500

    Writing JSON Files

--- ExternalDataProjectPart6/src/WritingJSONFiles.java
+++ ExternalDataProjectPart6/src/WritingJSONFiles.java
@@ -1,6 +1,6 @@
-import org.json.simple.*;
-import java.io.*;
-import java.util.Formatter;
+import com.google.gson.*;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by Lucas Chapman 6/16/2020.
@@ -8,68 +8,61 @@ import java.util.Formatter;
  */
 public class WritingJSONFiles {
     public static void main(String[] args) {
-        JSONObject device = createJSONObject();
-        JSONArray laptops = createJSONArray();
-        device.put("laptops", laptops);
-        writeFile(device);
+        Gson device = new GsonBuilder().setPrettyPrinting().create();
+        List<Laptop> laptops = laptops();
+        writeFile(device, laptops);
     }
 
     /**
-     * Creates the JSONObject that will contain all of the data in the JSON file
-     * @return returns the data stored in the JSONObject
+     * Creates Laptop objects and stores each object in a list
+     * @return returns the objects stored in the list
      */
-    private static JSONObject createJSONObject() {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("device","laptop");
-        return jsonObject;
-    }
-
-    /**
-     * Creates the JSONArray that will contain all the Laptop objects in the JSON file
-     * @return returns the data stored in the JSONArray
-     */
-    private static JSONArray createJSONArray() {
-        JSONArray jsonArray = new JSONArray();
+    private static List<Laptop> laptops() {
+        List<Laptop> lapList = new ArrayList<>();
 
         // 1st Laptop
-        JSONObject laptop1 = new JSONObject();
-        laptop1.put("id", 1010);
-        laptop1.put("name", "Business Surface Laptop 3");
-        laptop1.put("productNumber", "10JCR6UKR5");
-        laptop1.put("serial", "LKS67BX1");
-        laptop1.put("os", "Windows 10");
-        laptop1.put("ram", 999.9);
-        laptop1.put("cpu", "Intel Core i9-999GHz");
-        laptop1.put("storage", 999.9);
-        laptop1.put("price", 999999.99);
-        jsonArray.add(laptop1);
+        Laptop laptop1 = new Laptop();
+        laptop1.setId(1010);
+        laptop1.setBrand("Microsoft");
+        laptop1.setName("Business Surface Laptop 3");
+        laptop1.setProductNumber("10JCR6UKR5");
+        laptop1.setSerial("LKS67BX1");
+        laptop1.setOs("Windows 10");
+        laptop1.setRam(999.9);
+        laptop1.setCpu("Intel Core i9-999GHz");
+        laptop1.setStorage(999.9);
+        laptop1.setPrice(999999.99);
+        lapList.add(laptop1);
 
-        //2nd Laptop
-        JSONObject laptop2 = new JSONObject();
-        laptop2.put("id", 1011);
-        laptop2.put("name", "MacBook Air Pro");
-        laptop2.put("productNumber", "15MNW66KRS");
-        laptop2.put("serial", "3QOM1AAW");
-        laptop2.put("os", "macOS");
-        laptop2.put("ram", 50.5);
-        laptop2.put("cpu", "Intel Core i9-75GHz");
-        laptop2.put("storage", 246.1);
-        laptop2.put("price", 599.99);
-        jsonArray.add(laptop2);
-        return jsonArray;
+        // 2nd Laptop
+        Laptop laptop2 = new Laptop();
+        laptop2.setId(1011);
+        laptop2.setBrand("Apple");
+        laptop2.setName("MacBook Air Pro");
+        laptop2.setProductNumber("15MNW66KRS");
+        laptop2.setSerial("3QOM1AAW");
+        laptop2.setOs("macOS");
+        laptop2.setRam(50.5);
+        laptop2.setCpu("Intel Core i9-75GHz");
+        laptop2.setStorage(246.1);
+        laptop2.setPrice(599.99);
+        lapList.add(laptop2);
+        return lapList;
     }
 
     /**
      * Prints out the data created in this class and writes a JSON file
-     * @param device passes the JSONObject that will contain all the data in the JSON file
+     * @param device passes the Gson object that will contain all the data in the JSON file
+     * @param laptops passes the List that contains all the Laptop objects
      */
-    private static void writeFile(JSONObject device) {
+    private static void writeFile(Gson device, List<Laptop> laptops) {
         try {
-            // Converts all the data in the JSONObject to a String
-            StringWriter data = new StringWriter();
-            device.writeJSONString(data);
-            String jsonText = data.toString();
-            System.out.println(jsonText);   // displays all the data to the console for testing
+            // Converts the data to a String
+            String jsonText = "{\"device\":\"laptop\",";
+            jsonText += "\n \"laptops\":";
+            jsonText += device.toJson(laptops);
+            jsonText += "}";
+            System.out.println(jsonText);   // displayed to console for testing
 
             // Writes the JSON file with the data created in this class
             Formatter file = new Formatter("laptops2.json");
